01a10630af938157c25e7313e39bf2f779cd2d12
Sebastian Horton <36234258+SebastianH11@users.noreply.github.com>
2019-05-08 23:16:57 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BomberMan_2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        Player player1 = new Player();
+        Point player1Point = new Point(0, 0);
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            Map map = new Map();
+            map.initializeMap(canvas);
+            
+            player1.createPlayer(Brushes.DarkRed, canvas, player1Point);
+            gameTimer.Tick += gameTimer_Tick;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            gameTimer.Start();
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            player1.updatePlayer(player1Point);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BomberMan_2._0
+{
+    class Player
+    {
+        enum Direction { up, down, left, right};
+        static Direction direction;
+      
+        Map m = new Map();
+        Rectangle playerRectangle;
+        public Point updatePlayer(Point playerPoint)
+        {
+            
+            if(m.checkWalkable() == true)
+            {
+                if(Keyboard.IsKeyDown(Key.W))
+                {
+                    if(playerPoint.Y > 0)
+                    {
+                        playerPoint.Y -= 64;
+                    }
+                }
+                if(Keyboard.IsKeyDown(Key.S))
+                {
+                    if(playerPoint.Y < 512)
+                    {
+                        playerPoint.Y += 64;
+                    }
+                }
+            }
+            Canvas.SetTop(playerRectangle, playerPoint.Y);
+            Canvas.SetLeft(playerRectangle, playerPoint.X);
+            return playerPoint;
+        }
+        public void createPlayer(Brush colour, Canvas c, Point p)
+        {
+            playerRectangle = new Rectangle();
+            playerRectangle.Height = 64;
+            playerRectangle.Width = 64;
+            playerRectangle.Fill = colour;
+
+
+            c.Children.Add(playerRectangle);
+        }
+    }
+}
